a28170a5a42183aaf818129ecebc21efa256d900
Sonushavik <sonuk06212@gmail.com>
2024-10-20 21:18:33 +0530

    removes donutChart.css

--- src/DonutChart.jsx
+++ src/DonutChart.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { alignProperty } from '@mui/material/styles/cssUtils';
-import './DonutChart.css';
+
 
 // Register necessary components for Chart.js
 ChartJS.register(ArcElement, Tooltip, Legend);
